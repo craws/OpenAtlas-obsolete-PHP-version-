82e240162098abefdafa0ae94b22bbc2489b788c
Bernhard Koschicek <bkoschicek@IMAFO-BYZ-NB02.oeaw.ads>
2016-04-07 17:34:35 +0200

    adaptions for dpp

--- application/modules/default/controllers/FileController.php
+++ application/modules/default/controllers/FileController.php
@@ -7,7 +7,7 @@ class FileController extends Zend_Controller_Action {
         $this->getHelper('viewRenderer')->setNoRender(true);
         switch ($this->_getParam('file')) {
             case 'schema':
-                $image = file_get_contents(APPLICATION_PATH . '/../data/documentation/openatlas_schema.png');
+                $image = file_get_contents(APPLICATION_PATH . '/../data/documentation/openatlas_schema_dpp.png');
                 header('Content-type: image/jpg');
                 // @codeCoverageIgnoreStart
                 echo $image;
